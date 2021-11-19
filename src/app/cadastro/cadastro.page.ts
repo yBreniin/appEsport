1cb59d41c584ccc11116c73fff348fc6a2efadce
Etec 244 <e244ti@outlook.com>
2021-11-19 14:57:47 -0200

    appEsport

---
+++ src/app/cadastro/cadastro.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.page.html',
+  styleUrls: ['./cadastro.page.scss'],
+})
+export class CadastroPage implements OnInit {
+
+  formCadastro: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formCadastro = this.formBuilder.group({
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      cpf: ['', Validators.compose([Validators.required])],
+      dataNascimento: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])],
+      confirmaSenha: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  salvarCadastro(){
+    console.log('Formulário:  ', this.formCadastro.valid);
+  }
+
+}
